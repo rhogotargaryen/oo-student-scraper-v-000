b964a2b34b1f39e8776258dff87aa45bf412b5f8
Derek Haller <dhaller.personal@gmail.com>
2018-02-15 02:12:06 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,37 +2,54 @@ require 'open-uri'
 require 'pry'
 require 'nokogiri'
   class Scraper
-    attr_accessor :page_loc
-    @page_loc = "http://165.227.31.208:56222/fixtures/student-site/"
-    def self.index_to_param(index_url)
-      doc = Nokogiri::HTML(open(@page_loc))
-      studs_divs = doc.css(".student-card a h4")
-      studs_names = []
-      studs_divs.each do |stud_div|
-        studs_names << stud_div.text
-      end
-      studs_param = []
-      studs_names.each do |name|
-        name.gsub!(" ", "-")
-        name = "#{name}.html"
-        studs_param << name.downcase
-      end
-      studs_param
-    end
+
     def self.scrape_index_page(index_url)
-      studs_param = index_to_param(index_url)
-      students_hashes = []
-      studs_param.each do |param|
-        doc = Nokogiri::HTML(open("#{@page_loc}students/#{param}"))
-        stu = {}
-        stu[:profile_url] = param
-        stu[:name] = param.gsub("-", " ").gsub(".html", "").split.map(&:capitalize).join(' ')
-        stu[:location] = doc.css(".vitals-text-container h2").text binding.pry
-        students_hashes << stu
+      doc = Nokogiri::HTML(open(index_url))
+      student_hashes = []
+      stu = {}
+      studs_links = doc.css(".student-card a")
+      studs_links.each do |stud_link|
+        stu[:name] = stud_link.css("h4").text
+        stu[:profile_url] = stud_link.attr("href")
+        stu[:loaction] = stud_link.css("p").text
+        student_hashes << stu
       end
+      student_hashes
+
       binding.pry
-      students_hashes
+
     end
+
+    #def self.index_to_param(index_url)
+    #  doc = Nokogiri::HTML(open(index_url))
+    #  studs_divs = doc.css(".student-card a h4")
+    #  studs_names = []
+    #  studs_divs.each do |stud_div|
+    #    studs_names << stud_div.text
+    #  end
+    #  studs_param = []
+    #  studs_names.each do |name|
+    #    name.gsub!(" ", "-")
+    #    name = "#{name}.html"
+    #    studs_param << name.downcase
+    #  end
+    #  studs_param
+    #end
+    #def self.scrape_index_page(index_url)
+    #  studs_param = index_to_param(index_url)
+    #  students_hashes = []
+    #  studs_param.each do |param|
+    #    binding.pry
+    #    doc = Nokogiri::HTML(open("./fixtures/student-site/students/#{param}"))
+    #    stu = {}
+    #    stu[:profile_url] = param
+    #    stu[:name] = param.gsub("-", " ").gsub(".html", "").split.map(&:capitalize).join(' ')
+    #    stu[:location] = doc.css(".vitals-text-container h2").text
+    #    students_hashes << stu
+    #    binding.pry
+    #  end
+    #  students_hashes
+    #end
     def self.scrape_profile_page(profile_url)
     end
   end
